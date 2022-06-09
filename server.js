4d14766bd207a947ae03befc5e79bf5cf99ced7c
Vlad Yanovych <yanovich30@gmail.com>
2022-06-09 12:55:42 +0300

    setup for heroku deploy

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
+app.use(express.static(__dirname + '/dist/team-worker-client'));
+app.get('/*', function(req,res)
 
-app.use(express.static('./dist/team-worker-client'));
+{res.sendFile(path.join(__dirname+'/dist/team-worker-client/index.html'));});
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/team-worker-client/'}
-);
-});
-
-app.listen(process.env.PORT || 4200);
+  app.listen(process.env.PORT || 4200);
